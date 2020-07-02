f719b866bf32ee8b695a65b588fc6cfd09d26484
Nick Rafferty <nickrafferty78@gmail.com>
2020-07-02 14:21:44 -0500

    Scala refresher

--- src/main/scala/part1recap/ScalaRecap.scala
+++ src/main/scala/part1recap/ScalaRecap.scala
@@ -6,108 +6,96 @@ import scala.util.{Failure, Success}
 object ScalaRecap extends App {
 
   // values and variables
-  val aBoolean: Boolean = false
+  val aBoolean: Boolean = true
+  val andIfexpression = if(2>3) "bigger" else "smaller"
+  val theUnit = println("hello") //no meaningful value
+  def functionName(x:Int): Unit ={
+    42
+  }
 
-  // expressions
-  val anIfExpression = if(2 > 3) "bigger" else "smaller"
-
-  // instructions vs expressions
-  val theUnit = println("Hello, Scala") // Unit = "no meaningful value" = void in other languages
-
-  // functions
-  def myFunction(x: Int) = 42
-
-  // OOP
+  //oop
   class Animal
   class Cat extends Animal
   trait Carnivore {
-    def eat(animal: Animal): Unit
+    def eat(animal:Animal): Unit
   }
 
   class Crocodile extends Animal with Carnivore {
-    override def eat(animal: Animal): Unit = println("Crunch!")
+    def eat(animal:Animal): Unit=println("crunch")
+  }
+  //singleton
+  object MySingleton{
+
   }
 
-  // singleton pattern
-  object MySingleton
-
-  // companions
   object Carnivore
 
-  // generics
+  //generics
   trait MyList[A]
 
-  // method notation
-  val x = 1 + 2
+//method notation
+  val x= 1+2
   val y = 1.+(2)
 
-  // Functional Programming
-  val incrementer: Int => Int = x => x + 1
+  //Functional Programming
+  val incrementer: Int => Int = x => x+1
   val incremented = incrementer(42)
 
-  // map, flatMap, filter
-  val processedList = List(1,2,3).map(incrementer)
+  //map, flatmap and filter - all higher order functions, they can take in other functions as paramters
 
-  // Pattern Matching
-  val unknown: Any = 45
+  //Pattern matching
+  val unknown:Any = 45
   val ordinal = unknown match {
     case 1 => "first"
     case 2 => "second"
     case _ => "unknown"
   }
-
-  // try-catch
-  try {
+  //try catch
+  try{
     throw new NullPointerException
-  } catch {
-    case _: NullPointerException => "some returned value"
-    case _: Throwable => "something else"
+  }catch{
+    case e: NullPointerException => "Some value"
+    case _ => "something else"
   }
 
-  // Future
+  //Futures
   import scala.concurrent.ExecutionContext.Implicits.global
   val aFuture = Future {
-    // some expensive computation, runs on another thread
+    //some expensive computation, runs on another thread
     42
   }
-
   aFuture.onComplete {
-    case Success(meaningOfLife) => println(s"I've found $meaningOfLife")
-    case Failure(ex) => println(s"I have failed: $ex")
+    case Success(meaningOfLife) => println(s"Ive found the $meaningOfLife")
+    case Failure(ex) => println(s"oh no $ex")
   }
 
-  // Partial functions
+  //partial functions
   val aPartialFunction: PartialFunction[Int, Int] = {
-    case 1 => 43
-    case 8 => 56
-    case _ => 999
+    case 1 => 34
+    case _ => 99
   }
 
-  // Implicits
-
-  // auto-injection by the compiler
-  def methodWithImplicitArgument(implicit x: Int) = x + 43
+  //implicits - one of the most important part
+  //auto injected into the compiler
+  def methodwithImplicit(implicit x:Int) = x +43
   implicit val implicitInt = 67
-  val implicitCall = methodWithImplicitArgument
+//implicit call
+  methodwithImplicit
 
-  // implicit conversions - implicit defs
-  case class Person(name: String) {
-    def greet = println(s"Hi, my name is $name")
+  //implicit conversions
+  case class Person(name:String) {
+    def greet = println(s"Hi my name is $name")
+  }
+  implicit def fromStringToPerson(name:String) = Person(name)
+  "Bob".greet
+
+  //implicit conversion with implicit classes
+  implicit class Dog(name:String){
+    def bark = println("Bark")
   }
 
-  implicit def fromStringToPerson(name: String) = Person(name)
-  "Bob".greet // fromStringToPerson("Bob").greet
-
-  // implicit conversion - implicit classes
-  implicit class Dog(name: String) {
-    def bark = println("Bark!")
-  }
   "Lassie".bark
 
-  /*
-    - local scope
-    - imported scope
-    - companion objects of the types involved in the method call
-   */
+
 
 }
