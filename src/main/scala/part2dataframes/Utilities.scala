3d469ea12a7d89ad8a22e6fa4a0f5bb50721c107
Nick Rafferty <nickrafferty78@gmail.com>
2020-07-14 07:57:58 -0500

    Finished work on columns and expressions

--- src/main/scala/part2dataframes/Utilities.scala
+++ src/main/scala/part2dataframes/Utilities.scala
@@ -1,5 +1,11 @@
 package part2dataframes
 
-class Utilities {
+import org.apache.log4j.{Level, Logger}
+
+object Utilities {
+  def setupLogging() = {
+    val rootLogger = Logger.getRootLogger()
+    rootLogger.setLevel(Level.ERROR)
+  }
 
 }
